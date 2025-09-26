445adf07aa6418583b3e3d0b3983a45afd726c5a
dhjeong0 <dhjeong@markany.com>
2025-09-26 15:54:27 +0900

    Fix: BE코드 삭제, strands agent 연동

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://kej7nzklnb.execute-api.ap-northeast-2.amazonaws.com/dev',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })
